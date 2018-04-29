7cf3306cc3c6d554bf18ffbab4e44c579e3a4b3a
Chris Manning <cmanning@vt.edu>
2018-04-29 13:54:34 -0400

    PID::Init

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,15 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  // Initialize gains
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+  // Initialize errors
+  PID::p_error = 0.0;
+  PID::i_error = 0.0;
+  PID::d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
